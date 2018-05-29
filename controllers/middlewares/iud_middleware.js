1d3a26b85bc81fb4789a2477d5cf3907c316fb54
vinay <vinay.naik@outlook.com>
2018-05-29 12:57:41 +0530

    remove non utilized methods

--- controllers/middlewares/iud_middleware.js
+++ controllers/middlewares/iud_middleware.js
@@ -12,8 +12,7 @@ module.exports = function injectUserDataMiddleware (msbotController, msBot) {
     // check if user is already linked
     message.haystack_data = {};
     //await injectPromisedAsyncUserData(message);
-
-    await injectPlainUserData(message);
+    await injectPromisedUserData(message);
     console.log("injected user in to message now");
     // do not let this be asynchronously written; it might not be available for consumption at message in-time
     next();
@@ -43,75 +42,21 @@ module.exports = function injectUserDataMiddleware (msbotController, msBot) {
   /**
    * Resolve user presence through promise
    */
-  function injectPromisedUserData(message) {
+  async function injectPromisedUserData(message) {
     const userCheck = util.promisify(msbotController.storage.users.get);
-    userCheck(message.user).then((user) => {
-      // Do something with `user`
-      console.log("injectPromisedUserData");
-      //console.log(user);
+    await userCheck(message.user).then((user) => {
       if (!user) {
-        console.log("user is non existent apparently!");
         user = {
           id: message.user,
           linked_to_haystack: false
         };
         msbotController.storage.users.save(user, function(err, id) {});
-      } else {
-        console.log("user exists!");
       }
       message.haystack_data = user;
-      console.log("injection status is: ");
-      console.log(message.haystack_data);
     }).catch((error) => {
       // Handle the error.
       console.log(error);
     });
   }
-
-  /**
-   * Resolve user presence without promise but await
-   */
-  async function injectPlainAwaitUserData(message) {
-    msbotController.storage.users.get(message.user, function(err, user) {
-      console.log("callback user : ");
-      console.log(user);
-      if (!user) {
-        console.log("user is non existent apparently!");
-        user = {
-          id: message.user,
-          linked_to_haystack: true
-        };
-        //msbotController.storage.users.save(user, function(err, id) {});
-      } else {
-        console.log("user exists!");
-      }
-      message.haystack_data = user;
-      console.log(message.haystack_data);
-    });
-  }
-
-  /**
-   * Resolve user presence without promise
-   */
-  async function injectPlainUserData(message) {
-    var user = await msbotController.storage.users.get(message.user, function(err, user) {
-      console.log("callback user : ");
-      console.log(user);
-    });
-    console.log("injectUserData");
-    console.log(user);
-    if (!user) {
-      console.log("user is non existent apparently!");
-      user = {
-        id: message.user,
-        linked_to_haystack: false
-      };
-      //msbotController.storage.users.save(user, function(err, id) {});
-    } else {
-      console.log("user exists!");
-    }
-    message.haystack_data = user;
-    console.log(message.haystack_data);
-  }
-
+  
 }
