4c52591b829c47505bc62198debcbf39852226c7
vinay <vinay.naik@outlook.com>
2018-06-05 23:31:20 +0530

    realign unlink endpoint path to trumpet

--- controllers/endpoints/unlink_endpoint.js
+++ controllers/endpoints/unlink_endpoint.js
@@ -3,7 +3,7 @@ module.exports = function unlinkEndpoint(msbotControllerComplex, restServer) {
 
   // For unliniking messaging
   // curl -X POST -H "Content-Type: multipart/form-data;" -F "message=This is my message" http://localhost:8089/orator/unlink
-  restServer.post('/orator/channel/unlink', function unlinkUser(req, res, next) {
+  restServer.post('/trumpet/channel/unlink', function unlinkUser(req, res, next) {
     let unlinkModule = require(__dirname + '/../providers/unlink_provider.js');
     unlinkModule(msbotControllerComplex.controller, msbotControllerComplex.bot, req.body);
 
