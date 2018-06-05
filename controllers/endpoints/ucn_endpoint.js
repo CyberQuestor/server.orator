8ccf80424e324bac530b5a8169446978ec69d12f
vinay <vinay.naik@outlook.com>
2018-06-05 23:30:59 +0530

    realign path to trumpet for ucn

--- controllers/endpoints/ucn_endpoint.js
+++ controllers/endpoints/ucn_endpoint.js
@@ -2,7 +2,7 @@
 module.exports = function userConnectNotificationEndpoint(msbotControllerComplex, restServer) {
 
   // For user connect notification
-  restServer.post('/orator/channel/ucn', function userConnectNotification(req, res, next) {
+  restServer.post('/trumpet/channel/ucn', function userConnectNotification(req, res, next) {
     console.log("userConnectNotification endpoint hit");
     let ucnModule = require(__dirname + '/../providers/ucn_provider.js');
     ucnModule(msbotControllerComplex.controller, msbotControllerComplex.bot, req.body);
