30abdc3bd16100b0fd74a3270a1e2eae78cd56e9
vinay <vinay.naik@outlook.com>
2018-06-06 18:46:22 +0530

    add split parameter for discovered secret

--- utilities/interpreter/access.js
+++ utilities/interpreter/access.js
@@ -1,20 +1,22 @@
 const global_exec_sync = require('child_process').execSync;
 const global_util = require('util');
 
+let path = process.env.haystack_orator_redis_utilities_path;
+let cls = process.env.haystack_orator_redis_utilities_class;
+let parameter = process.env.haystack_orator_redis_utilities_class_parameter;
+let key = process.env.haystack_orator_redis_cred_key;
+let loc = process.env.haystack_orator_redis_cred_loc;
+let separator = process.env.haystack_orator_redis_separator;
 
-module.exports = function access_database () {
+let compileIt = 'java -cp ' + path + ' ' + cls + ' ' + parameter + ' "' + key + '" ' + loc;
 
-  let path = process.env.haystack_orator_redis_utilities_path;
-  let cls = process.env.haystack_orator_redis_utilities_class;
-  let parameter = process.env.haystack_orator_redis_utilities_class_parameter;
-  let key = process.env.haystack_orator_redis_cred_key;
-  let loc = process.env.haystack_orator_redis_cred_loc;
+//await prepare_access();
+let secret = global_exec_sync(compileIt).toString();
 
-  let compileIt = 'java -cp ' + path + ' ' + cls + ' ' + parameter + ' "' + key + '" ' + loc;
+if(secret) {
+  let splicedSecret = secret.split(separator);
+  if(splicedSecret && splicedSecret.length > 0) {
+    process.env['redsec'] = splicedSecret[1];
+  }
 
-  console.log('child process beginning ...');
-  console.log(compileIt);
-
-  //await prepare_access();
-  let secret = global_exec_sync(compileIt).toString();
 }
