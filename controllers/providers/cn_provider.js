05b91afc1367deb0f4ace5d19f78181bc7a22e4f
vinay <vinay.naik@outlook.com>
2018-07-24 16:34:39 +0530

    Add support for extended notifications part one

--- controllers/providers/cn_provider.js
+++ controllers/providers/cn_provider.js
@@ -17,8 +17,29 @@ module.exports = function commonNotificationProvider(msbotController, bot, stora
     case 'common_notification_provider_pov_share_body':
       contextShareNotification(bunchOfArguments, messageKey);
       break;
+    case 'common_notification_provider_pov_prompt_body':
+    case 'common_notification_provider_pov_recommend_body':
+    case 'common_notification_provider_pov_show_disinterest_body':
+    case 'common_notification_provider_pov_show_interest_body':
+    case 'common_notification_provider_pov_questionnaire_followup_body':
+    case 'common_notification_provider_license_approved_body':
+    case 'common_notification_provider_license_rejected_body':
+      contextCommonNotification(bunchOfArguments, messageKey);
+      break;
     default:
-    break;
+      break;
+  }
+
+  function contextCommonNotification(bunchOfArguments, messageKey){
+    let simpleText = bot.i18n.__mf({phrase: messageKey, locale}, bunchOfArguments);
+
+    // now say a few words
+    bot.say(
+      {
+        text: simpleText,
+        address: extractedPrefix.address
+      }
+    );
   }
 
   function contextShareNotification(bunchOfArguments, messageKey){
@@ -32,4 +53,5 @@ module.exports = function commonNotificationProvider(msbotController, bot, stora
       }
     );
   }
+
 }
