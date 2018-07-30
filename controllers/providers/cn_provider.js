05605246a4a5e88ab93d5bc5b8fd3c4e6ce82c66
vinay <vinay.naik@outlook.com>
2018-07-30 17:15:19 +0530

    Add support for whatsnew broadcast

--- controllers/providers/cn_provider.js
+++ controllers/providers/cn_provider.js
@@ -33,6 +33,7 @@ module.exports = function commonNotificationProvider(msbotController, bot, stora
     case 'common_notification_provider_import_failure_partial_body':
     case 'common_notification_provider_import_failure_all_body':
     case 'common_notification_provider_import_success_body':
+    case 'common_notification_provider_whatsnew_broadcast_body':
       contextCommonNotification(bunchOfArguments, messageKey);
       break;
     default:
