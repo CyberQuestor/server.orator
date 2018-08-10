b376244f567bfca7b5652d0c7aa92b559a26591b
vinay <vinay.naik@outlook.com>
2018-08-10 16:08:54 +0530

    reorganize web bot initialization

--- bot.js
+++ bot.js
@@ -10,6 +10,26 @@ const logger_init = require(__dirname + '/utilities/interpreter/logger.js').init
 const logger = reqlib('/utilities/interpreter/logger.js').fetchLogger(__dirname + __filename);
 const ResponseCode = reqlib('/utilities/interpreter/rode.js').fetchResponseCode();
 
+
+
+// Set up bot framework for extended reach
+var msbotControllerComplex = require(__dirname + '/components/botframework.js')();
+
+// Set up restify-powered server to expose Haystack facing endpoints
+var restServer = require(__dirname + '/components/trumpet.js')(msbotControllerComplex);
+
+// Load in a plugin that defines the bot's identity
+require(__dirname + '/components/plugin_identity.js')(msbotControllerComplex.controller);
+
+var normalizedPath = require("path").join(__dirname, "controllers/skills/botframework");
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  require("./controllers/skills/botframework/" + file)(msbotControllerComplex.controller);
+});
+
+/****************************************************************************************
+** Section for embedded web bot
+*****************************************************************************************/
+
 var Botkit = require('botkit');
 var debug = require('debug')('botkit:main');
 
@@ -30,48 +50,34 @@ if (process.env.haystack_orator_redis_activate && process.env.haystack_orator_re
 }
 
 // Create the Botkit controller, which controls all instances of the bot.
-//var controller = Botkit.socketbot(bot_options);
+var controller = Botkit.socketbot(bot_options);
 
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
-//var webserver = require(__dirname + '/components/express_webserver.js')(controller);
-
-// Set up bot framework for extended reach
-var msbotControllerComplex = require(__dirname + '/components/botframework.js')();
-
-// Set up restify-powered server to expose Haystack facing endpoints
-var restServer = require(__dirname + '/components/trumpet.js')(msbotControllerComplex);
-
-
+var webserver = require(__dirname + '/components/express_webserver.js')(controller);
 
 // Load in some helpers that make running Botkit on Glitch.com better -- not required, self hosted
 //require(__dirname + '/components/plugin_glitch.js')(controller);
 
 // Load in a plugin that defines the bot's identity
-//require(__dirname + '/components/plugin_identity.js')(controller);
-// Load in a plugin that defines the bot's identity
-require(__dirname + '/components/plugin_identity.js')(msbotControllerComplex.controller);
+require(__dirname + '/components/plugin_identity.js')(controller);
 
 // enable advanced botkit studio metrics
 // and capture the metrics API to use with the identity plugin!
-//controller.metrics = require('botkit-studio-metrics')(controller);
+controller.metrics = require('botkit-studio-metrics')(controller);
 
 // Open the web socket server
-//controller.openSocketServer(controller.httpserver);
+controller.openSocketServer(controller.httpserver);
 
 // Start the bot brain in motion!!
-//controller.startTicking();
+controller.startTicking();
 
 
-// var normalizedPath = require("path").join(__dirname, "controllers/skills/web");
-// require("fs").readdirSync(normalizedPath).forEach(function(file) {
-//   require("./controllers/skills/web/" + file)(controller);
-// });
-var normalizedPath = require("path").join(__dirname, "controllers/skills/botframework");
+var normalizedPath = require("path").join(__dirname, "controllers/skills/web");
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./controllers/skills/botframework/" + file)(msbotControllerComplex.controller);
+ require("./controllers/skills/web/" + file)(controller);
 });
 
-//console.log('I AM ONLINE! COME TALK TO ME: http://localhost:' + (process.env.PORT || 3000))
+logger.silly('I AM ONLINE! COME TALK TO ME: http://localhost:' + (process.env.PORT || 3000))
 
 if (process.env.studio_token) {
   // not used
